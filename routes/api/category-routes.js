b006712c12fea1d126129adde3d95f198dffa45a
Julio C. MW11 <sysa.mail@gmail.com>
2022-12-07 17:21:13 -0600

    se agrega endpoint PUT categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,8 +46,21 @@ router.post('/', async (req, res) => {
 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+    const result = await Category.update({
+      category_name:req.body.category_name
+    },
+    {where :{id: req.params.id}
+  })
+  res.status(200).json(result);
+
+  }
+  catch (error){
+    res.status(500).json(error);
+  }
+
 });
 
 router.delete('/:id', (req, res) => {
